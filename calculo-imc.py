cada357c08166c703ab2775a129783f955927752
Bruno Pergentino <brunovpergentino@gmail.com>
2022-09-14 20:19:09 -0300

    teste

---
+++ calculo-imc.py
@@ -0,0 +1,20 @@
+############### CALCULO IMC ##################
+
+def IMC():
+    while True:
+        massa = float(input("Digite uma massa em kg: "))
+        altura = float(input("Digite uma altura em cm: "))
+        imc = massa/(altura**2)
+
+        print(f"Seu IMC é: {imc*10000}")
+        
+        print('seu cu mlk')
+        <20
+        25
+        30
+        35
+        40
+        45
+    
+
+IMC()
