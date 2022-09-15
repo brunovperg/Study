add938ec3585253a70cca3ea1aeef662def60bd3
Bruno Pergentino <brunovpergentino@gmail.com>
2022-09-15 19:20:24 -0300

    Add print

--- calculo-imc.py
+++ calculo-imc.py
@@ -3,18 +3,23 @@
 
 def IMC():
     while True:
-        massa = float(input("Digite uma massa em kg: "))
+        massa = float(input("\nDigite uma massa em kg: "))
         altura = float(input("Digite uma altura em cm: "))
-        imc = massa/(altura**2)
+        imc = (massa/(altura**2))*10000
 
-        print(f"Seu IMC é: {imc*10000}")
+        print(f"\nSeu IMC é: {round(imc, 2)}")
         
-        print('seu cu mlk')
-        <20
-        25
-        30
-        35
-        40
-        45
-    
-print("Alesgr")
+        if imc < 18.5:
+            print("\nVocê está abaixo do peso ideal")
+        elif 18.5 <= imc < 25:
+            print("\nVocê está no peso ideal")
+        elif 25 <= imc < 30:
+            print("\nVocê está acima do peso ideal")
+        elif 30 <= imc < 35:
+            print("\nVocê está com Obesidade Grau I")
+        elif 35 <= imc < 40:
+            print("\nVocê está com Obesidade Grau II")
+        else:
+            print("\nVocê está com Obesidade Grau III")
+
+IMC()
