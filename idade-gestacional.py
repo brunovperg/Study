db5f20239990124181b58490716f36488a275376
Bruno Pergentino <brunovpergentino@gmail.com>
2022-09-19 11:58:08 -0300

    Changes on commands

--- idade-gestacional.py
+++ idade-gestacional.py
@@ -1,6 +1,5 @@
 ############################# CALCULADORA IDADE GESTACIONAL #########################
 from datetime import date, datetime
-controle = 1
 while True:
     print('\n-------------------------------------------')
     print('\n ######## CALCULADORA IDADE GESTACIONAL #####')
@@ -11,9 +10,9 @@ while True:
     if controle == 1:
         print('\n-------------------------------------------')
         print('\nCÁLCULO PELA ULTIMA MENSTRUAÇÃO')
-        lastPeriodYear = int(input("\nAno da ultima menstruação (AAAA): "))
-        lastPeriodMonth = int(input("\nMês da ultima menstruação (MM): "))
         lastPeriodDay = int(input("\nDia da ultima menstruação: (DD): "))
+        lastPeriodMonth = int(input("\nMês da ultima menstruação (MM): "))
+        lastPeriodYear = int(input("\nAno da ultima menstruação (AAAA): "))
 
         LastPeriod = datetime.strptime(f'{lastPeriodYear}{lastPeriodMonth}{lastPeriodDay}', "%Y%m%d")
         today = date.today()
@@ -26,20 +25,14 @@ while True:
         weekTotal = (int(weeksOrdinal/7))
         print('\n-------------------------------------------')
         print(f"\nA idade gestacional é de {weekTotal} semanas e {daysTotal} dias.")
-        print('\n-------------------------------------------')
-        
-        print('\n1 - Repetir cálculo pela última menstruação')
-        print('\n2 - Fazer cálculo pelo último ultrasom')
-        controle = int(input('\nDigite um valor: '))
-        
         
      #  ####### CÁLCULO PELO ULTIMO ULTRASOM ######
     elif controle == 2:
         print('\n-------------------------------------------')
         print('\nCÁLCULO PELO ULTIMO ULTRASOM')
-        ultraYear = int(input("\nAno do ultimo ultrasom (AAAA): "))
-        ultraMonth = int(input("\nMês do ultimo ultrasom (MM): "))
         ultraDay = int(input("\nDia do ultimo ultrasom (DD): "))
+        ultraMonth = int(input("\nMês do ultimo ultrasom (MM): "))
+        ultraYear = int(input("\nAno do ultimo ultrasom (AAAA): "))
         
         weeksAdd = int(input('\nQuantas semanas tinham no ultrasom: '))*7
         dayAdd = int(input('\nQuantos dias tinham no ultrasom: '))
@@ -57,11 +50,6 @@ while True:
         
         print('\n-------------------------------------------')
         print(f"\nA idade gestacional é de {dateWeek} semanas e {dateDay} dias.")
-        print('\n-------------------------------------------')
-        
-        print('\n1 - Fazer cálculo pela última menstruação')
-        print('\n2 - Repetir cálculo pelo último ultrasom')
-        controle = int(input('\nDigite um valor: '))
         
     else:
         print('-------------------------------------------------')
